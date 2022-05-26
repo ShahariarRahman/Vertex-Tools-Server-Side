6cae34f8a70dd673c8f7464a942c100aadecba0c
ShahariarRahman <shahariarrahman98@gmail.com>
2022-05-26 21:58:59 +0600

    Db connect

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4xsemh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
+
+
+const run = async () => {
+    try {
+        await client.connect();
+        const toolCollection = client.db("vertex-tools").collection("tools");
+
+
+    }
+
+    finally { }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
     res.send('Vertex Tools Server Is Running')
 });
